585579999308c2df7c818014602d5ebdbf9b4d65
Stephen Wong <wongstephenky@gmail.com>
2022-06-22 11:07:27 -0700

    File structure setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const root = document.querySelector('#root')
+const spaceId = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+]
---
+++ planning/notes.md
@@ -0,0 +1,5 @@
+//made ui
+//assign id to each grid
+/figure out key values
+//figure out winning conditions
+//figure out turn
