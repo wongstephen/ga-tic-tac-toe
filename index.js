e37de77ceca63370409700143f992875a8ba82d0
Stephen Wong <wongstephenky@gmail.com>
2022-06-22 12:03:54 -0700

    Basic layout

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const root = document.querySelector('#root')
-const spaceId = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-]
+// X <i class="fa-solid fa-xmark"></i>
+// O <i class="fa-solid fa-o"></i>
+
+const game = document.querySelector("#game");
+
+(function () {
+  const spaceId = ["a", "b", "c", "d", "e", "f", "g", "h", "i"].forEach(
+    (id) => {
+      const div = document.createElement("div");
+      div.classList.add("square");
+      div.setAttribute("id", id);
+      div.setAttribute("data-tile", "empty");
+      div.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+      game.appendChild(div);
+    }
+  );
+})();
+
+const tileX = () => {};
